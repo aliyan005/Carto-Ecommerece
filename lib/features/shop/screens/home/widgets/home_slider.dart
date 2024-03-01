3f6a289e1b37f293142f311e88720b96ada0e468
Aliyan ahmed Azad <aliyanahmed853@gmail.com>
2024-03-02 01:08:33 +0500

    updated carto

--- lib/features/shop/screens/home/widgets/home_slider.dart
+++ lib/features/shop/screens/home/widgets/home_slider.dart
@@ -3,7 +3,6 @@ import 'package:ecommerece/common/widgets/custom_shapes/containers/custom_circle
 import 'package:ecommerece/common/widgets/images/rounded_image.dart';
 import 'package:ecommerece/features/shop/controllers/home_controller.dart';
 import 'package:ecommerece/utils/constants/colors.dart';
-import 'package:ecommerece/utils/constants/image_string.dart';
 import 'package:ecommerece/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
